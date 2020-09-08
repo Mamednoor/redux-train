6ac8961fa03a085cc469bd452654e16b11589a4b
Mamednoor <mamed.gohabur@gmail.com>
2020-09-08 23:34:25 +0200

    First commit learning redux in progress with a Todo list

---
+++ src/actions/todosAction.js
@@ -0,0 +1,18 @@
+import * as types from '../constants/TodoActionTypes'
+
+// définition des actions creators
+
+export const addTodo = (text, cat = 'default') => ({
+  type: types.ADD_TODO,
+  text,
+  cat
+})
+export const editTodoCat = (id, cat) => ({
+  type: types.EDIT_TODO_CAT,
+  id,
+  cat
+})
+export const setVisibilityFilter = (filter) => ({
+  type: types.SET_VISIBILITY_FILTER,
+  filter
+})
